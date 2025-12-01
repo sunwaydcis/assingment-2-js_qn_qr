db4152da0dbb4e3482f7d5c3bee69c00bab50e9b
JiaSan-123 <jiasan.sunway@gmail.com>
2025-12-02 03:15:31 +0800

    Improved code reusability & reduce redundancy

--- src/main/scala-3/MainApp.scala
+++ src/main/scala-3/MainApp.scala
@@ -1,11 +1,17 @@
 
 @main def run() =
-  val bookings = DataUtils.loadBookings()
+  val bookings = DataUtils.loadBookings("Hotel_Dataset.csv")
+  val analysisTasks = List(Question1(), Question2(), Question3())
 
-  Question1().run(bookings)
+  analysisTasks.foreach( question =>
+    question.run(bookings)
+    println()
+  )
 
-  println()
-  Question2().run(bookings)
-
-  println()
-  Question3().run(bookings)
+//  Question1().run(bookings)
+//
+//  println()
+//  Question2().run(bookings)
+//
+//  println()
+//  Question3().run(bookings)
